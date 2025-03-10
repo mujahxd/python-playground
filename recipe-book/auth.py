7c040bcba6d5f030f80f3e0d6a0f483580983b38
Mujahid <mujahid.170997@gmail.com>
2025-03-10 13:41:47 +0700

    add user authentication with username validation and password hashing

--- recipe-book/auth.py
+++ recipe-book/auth.py
@@ -1,6 +1,46 @@
 # auth.py (Authentication Management)
+from storage import load_data, save_data
+import utils
+
 def register():
-    pass  # Register implementation here
+    """Register a new user with username and password."""
+    users = load_data("users.json")
+
+
+    while True:
+        username = input("Enter a username: ")
+        if username in users:
+            print("Username already exists. Try another.")
+        else:
+            break
+    
+    while True:
+        password = input("Enter a password: ")
+        confirm_password = input("Confirm your password: ")
+
+        if password != confirm_password:
+            print("Passwords do not match. Try again.")
+        else:
+            break
+    
+    hashed_password = hash_password(password)
+    users[username] = hashed_password
+    save_data("users.json")
+    print("Registration successful!")
+
 
 def login():
-    pass  # Login implementation here
+    """Login a user by verifying username and password."""
+    users = load_data("users.json")
+
+    while True:
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+
+        if username not in users or not verify_password(password, users[username]):
+            print("Invalid username or password.")
+            return None
+        else:
+            print("Login successfull!")
+            return username
+    
