cdf634c14811d29a26fa357fabe788b1b4f4dc22
Mujahid <mujahid.170997@gmail.com>
2025-03-10 13:09:31 +0700

    add functions to load and save data with error handling

--- recipe-book/storage.py
+++ recipe-book/storage.py
@@ -1,6 +1,19 @@
 # storage.py (JSON Storage Management)
+import json
+import os
+
 def load_data(file):
-    pass  # Function to read JSON
+    """Load data from a JSON file. if file doesn't exist, return an emtpy dictionary."""
+    if not os.path.exists(file):
+        return {}
+    try:
+        with open(file, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except(json.JSONDecodeError, FileNotFoundError):
+        return {}
+
 
 def save_data(file, data):
-    pass  # Function to save JSON
+    """Save data to a JSON file."""
+    with open(file, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
